49f493ed0620e6e42eb0ccdcd1d4b5c9c1b01dde
V1T0bh <vitobenedicthalim@gmail.com>
2025-08-27 14:52:41 +0800

    Level-0 Meet Yoda delete Duke.java add Yoda.java

---
+++ src/main/java/Yoda.java
@@ -0,0 +1,10 @@
+public class Yoda {
+    public static void main(String[] args) {
+        System.out.println("Greetings youngling, Yoda is my name");
+        System.out.println("What help shall I do you for?");
+
+        System.out.print("\n");
+
+        System.out.println("Do or do not, I shall say goodbye.");
+    }
+}
