ee56665b9f3a860dcc49bda32bde820afd2c448b
V1T0bh <vitobenedicthalim@gmail.com>
2025-08-31 22:49:54 +0800

    Level-3: add Mark and Unmark feat
    add setLabel, print, mark, unmark methods in Task class
    add mark and unmark actions
    fix task lists

--- src/main/java/Yoda.java
+++ src/main/java/Yoda.java
@@ -1,33 +1,82 @@
 import java.util.Scanner;
 
 public class Yoda {
+    public static class Task {
+        public String label = "";
+        public boolean isDone = false;
+
+        public Task(String inputLabel) {
+            setLabel(inputLabel);
+        }
+
+        public void setLabel(String inputLabel) {
+            label = inputLabel;
+        }
+
+        public void print() {
+            String mark = (isDone) ? "X" : " ";
+            System.out.print("["+mark+"] ");
+            System.out.println(label);
+        }
+
+        public void mark() {
+            isDone = true;
+            System.out.println("Affirmative! Marked have been the task:");
+            print();
+        }
+
+        public void unmark() {
+            isDone = false;
+            System.out.println("Oh no, I shall unmark thy task:");
+            print();
+        }
+    }
+
     // function to output a greeting
     public static void greet() {
         System.out.println("Greetings youngling, Yoda is my name");
     }
 
     // function to ask the user for an input
-    /* if "bye", breaks the loop and terminates function
-    * if "list", shows previous inputs as a numbered list
-    * otherwise, adds to a list */
     public static void ask() {
+        // Initialize input variable, empty task list and item count
         final Scanner SCANNER = new Scanner(System.in);
-        String[] inputList = new String[100];
+        Task[] inputList = new Task[100];
         int count = 0;
 
         System.out.println("What help shall I do you for?");
 
+        /* if "bye", loop terminates
+         * if "list", shows previous inputs as a numbered list
+         * otherwise, adds to a list */
         String userInput =  SCANNER.nextLine();
         while (!userInput.equals("bye")) {
             if (userInput.equals("list")) {
+                if (count == 0) {
+                    System.out.println("This list of yours looks empty...");
+                }
                 for (int i = 0; i < count; i++) {
                     System.out.print((i+1) + ". ");
-                    System.out.println(inputList[i]);
+                    inputList[i].print();
+                }
+            } else if (userInput.split(" ")[0].equals("mark")) {
+                int itemId = Integer.parseInt(userInput.split(" ")[1]) - 1;
+                if (itemId < 0 & itemId >= count){
+                    System.out.println("Funny. This ID matches no task of yours.");
+                } else {
+                    inputList[itemId].mark();
+                }
+            } else if (userInput.split(" ")[0].equals("unmark")) {
+                int itemId = Integer.parseInt(userInput.split(" ")[1]) - 1;
+                if (itemId < 0 & itemId >= count){
+                    System.out.println("Funny. This ID matches no task of yours.");
+                } else {
+                    inputList[itemId].unmark();
                 }
             } else {
                 System.out.print("added: ");
                 System.out.println(userInput);
-                inputList[count] = userInput;
+                inputList[count] = new Task(userInput);
                 count += 1;
             }
             userInput = SCANNER.nextLine();
