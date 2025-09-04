a5500d65c19ea3b6c576a86580d7c6b6e4526177
V1T0bh <vitobenedicthalim@gmail.com>
2025-09-04 15:33:22 +0800

    refactor code for code quality
    line 7 - define magic number
    line 113 - avoid complicated expressions
    line 197,203,208 - abstraction
    line 195to212 - change if else to switch statement for simpler code

--- src/main/java/Yoda.java
+++ src/main/java/Yoda.java
@@ -91,6 +91,7 @@ public class Yoda {
         }
     }
 
+
     // function to split the input to array
     // [TASK TYPE, TASK LABEL, TASK START, TASK END], if exists
     public static String[] processInput(String userInput){
@@ -107,7 +108,9 @@ public class Yoda {
         // LABEL, START, END
         int i = 1;
         for (int j = 1; j < 4; j ++) {
-            while (!(i >= splitArray.length || splitArray[i].contains("/"))) {
+            boolean hasExceedLength = i >= splitArray.length;
+            boolean wordContainsSlash = splitArray[i].contains("/");
+            while (!(hasExceedLength || wordContainsSlash)) {
                 finalArray[j] += splitArray[i] + " ";
                 i += 1;
             }
@@ -120,73 +123,88 @@ public class Yoda {
         return finalArray;
     }
 
+    // function that takes in a task list and count
+    // prints out the list (if not empty)
+    // prints out a message (if empty)
+    public static void printTaskList(Task taskList[], int count){
+        if (count == 0) {
+            System.out.println("This list of yours looks empty...");
+        } else {
+            System.out.println("You have " + count + " tasks:");
+            for (int i = 0; i < count; i++) {
+                System.out.print((i + 1) + ". ");
+                System.out.println(taskList[i]);
+            }
+        }
+    }
 
-    // function to output a greeting
-    public static void greet() {
-        System.out.println("Greetings youngling, Yoda is my name");
+    // function that marks a task in a task list, depending on index
+    // prints a success message if task is found
+    public static void markTaskList(Task taskList[], int count, int index, boolean isMark){
+        if (index < 0 & index >= count){
+            System.out.println("Funny. This ID matches no task of yours.");
+        } else {
+            taskList[index].setMark(isMark);
+        }
+    }
+
+    // function that adds a task to a task list
+    // prints either a success or error message
+    // returns the new value of count
+    public static int addTaskList(Task[] taskList, String[] keywordsArray, int count){
+        if (count >= MAX_TASKS){
+            System.out.println("My condolences, it seems you have too much on your plate.");
+            System.out.println("Your task was not added.");
+        } else {
+            Task newTask = new Task();
+            switch (keywordsArray[0]){
+                case "todo":
+                    newTask = new Todo(keywordsArray[1]);
+                    break;
+                case "deadline":
+                    newTask = new Deadline(keywordsArray[1], keywordsArray[2]);
+                    break;
+                case "event":
+                    newTask = new Event(keywordsArray[1], keywordsArray[2], keywordsArray[3]);
+                    break;
+            }
+            taskList[count] = newTask;
+            System.out.println("Successfully added: ");
+            System.out.println(taskList[count]);
+            count += 1;
+        }
+        return count;
     }
 
     // function to ask the user for an input
     public static void ask() {
-
         // Initialize input variable, empty task list and item count
         Task[] inputList = new Task[MAX_TASKS];
         int count = 0;
 
-
-
         /* if "bye", loop terminates
          * if "list", shows previous inputs as a numbered list
-         * otherwise, adds to a list */
+         * if "mark"/"unmark", mark/unmark task accordingly
+         * if "deadline"/"todo"/"event", add task accordingly
+         */
         System.out.print("Yoda. Do or do not what shall I help you with? > ");
         String userInput =  SCANNER.nextLine();
         while (!userInput.equals("bye")) {
             String[] keywordsArray = processInput(userInput);
             switch (keywordsArray[0]){
                 case "list":
-                    if (count == 0) {
-                        System.out.println("This list of yours looks empty...");
-                    } else {
-                        System.out.println("You have " + count + " tasks:");
-                        for (int i = 0; i < count; i++) {
-                            System.out.print((i + 1) + ". ");
-                            System.out.println(inputList[i]);
-                        }
-                    }
+                    printTaskList(inputList, count);
                     break;
                 case "mark":
                 case "unmark":
                     int itemId = Integer.parseInt(keywordsArray[1]) - 1;
-                    if (itemId < 0 & itemId >= count){
-                        System.out.println("Funny. This ID matches no task of yours.");
-                    } else {
-                        inputList[itemId].setMark(keywordsArray[0].equals("mark"));
-                    }
+                    boolean isMark = keywordsArray[0].equals("mark");
+                    markTaskList(inputList, count, itemId, isMark);
                     break;
                 case "todo":
                 case "deadline":
                 case "event":
-                    if (count >= MAX_TASKS){
-                        System.out.println("My condolences, it seems you have too much on your plate.");
-                        System.out.println("Your task was not added.");
-                    } else {
-                        Task newTask = new Task();
-                        switch (keywordsArray[0]){
-                            case "todo":
-                                newTask = new Todo(keywordsArray[1]);
-                                break;
-                            case "deadline":
-                                newTask = new Deadline(keywordsArray[1], keywordsArray[2]);
-                                break;
-                            case "event":
-                                newTask = new Event(keywordsArray[1], keywordsArray[2], keywordsArray[3]);
-                                break;
-                        }
-                        inputList[count] = newTask;
-                        System.out.println("Successfully added: ");
-                        System.out.println(inputList[count]);
-                        count += 1;
-                    }
+                    count = addTaskList(inputList, keywordsArray, count);
                     break;
                 default:
                     System.out.println("Your instruction is invalid.");
@@ -199,7 +217,7 @@ public class Yoda {
     // main function that runs all other sub-functions
     public static void main(String[] args) {
         System.out.println("------------- YODA AWAKENS -------------");
-        greet();
+        System.out.println("Greetings youngling, Yoda is my name");
         ask();
 
         System.out.print("\n");
