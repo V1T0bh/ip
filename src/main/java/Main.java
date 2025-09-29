81c1847b0acdf932084e91fb9544de33fcb10b10
Vito Benedict Halim <vitobenedicthalim@gmail.com>
2025-09-30 00:18:49 +0800

    merge branch 6 Main file use ArrayList instead of conventional array add Delete method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import javax.naming.InsufficientResourcesException;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.util.Scanner;
+import java.util.ArrayList;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
@@ -11,6 +12,7 @@ public class Main {
     static final Scanner SCANNER = new Scanner(System.in); //  used for user input
     static final int MAX_TASKS = 100; // max no. of tasks
 
+    static ArrayList<Task> inputList = new ArrayList<>();
     private static int tasksArrLength;
 
     // function to split the input to array
@@ -51,32 +53,34 @@ public class Main {
     // function that takes in a task list and count
     // prints out the list (if not empty)
     // prints out a message (if empty)
-    private static void printTaskList(Task[] taskList, int count) {
-        if (count == 0) {
+    public static void printTaskList(ArrayList<Task> taskList) {
+        if (taskList.size() == 0) {
             System.out.println("This list of yours looks empty...");
         } else {
-            System.out.println("You have " + count + " tasks:");
-            for (int i = 0; i < count; i++) {
+            System.out.println("You have " + taskList.size() + " tasks:");
+            for (int i = 0; i < taskList.size(); i++) {
                 System.out.print((i + 1) + ". ");
-                System.out.println(taskList[i]);
+                System.out.println(taskList.get(i));
             }
         }
     }
 
     // function that marks a task in a task list, depending on index
     // prints a success message if task is found
-    private static void markTaskList(Task[] taskList, int count, int index, boolean isMark) {
-        if (index < 0 & index >= count) {
+    public static void markTaskList(ArrayList<Task> taskList, int index, boolean isMark) {
+        if (index < 0 | index >= taskList.size()) {
             System.out.println("Funny. This ID matches no task of yours.");
         } else {
-            taskList[index].setMark(isMark);
+            Task newTask = taskList.get(index);
+            newTask.setMark(isMark);
+            taskList.set(index, newTask);
         }
     }
 
     // function that adds a task to a task list
     // prints either a success or error message
     // returns the new value of count
-    private static int addTaskList(Task[] taskList, String[] keywordsArray, int count, boolean show) {
+    private static void addTaskList(String[] keywordsArray, boolean show) {
         try {
             int lastIndex;
             Task newTask;
@@ -99,18 +103,19 @@ public class Main {
                 default:
                     throw new InvalidCommandException(keywordsArray[0] + " is not a valid command...");
             }
-            taskList[count] = newTask;
+            inputList.add(newTask);
             if (show) {
                 // expected: entry through user input
                 System.out.println("Successfully added: ");
-                System.out.println(taskList[count]);
+                System.out.println(inputList.get(inputList.size() - 1));
             } else {
                 // expected: entry through file input
                 if (keywordsArray[lastIndex].equals("X")) {
-                    taskList[count].setMarkSilent(true);
+                    newTask = inputList.get(inputList.size() - 1);
+                    newTask.setMarkSilent(true);
+                    inputList.set(inputList.size()-1, newTask);
                 }
             }
-            count += 1;
         } catch (InsufficientResourcesException e) {
             System.out.print("SHEESHHH!! ");
             System.out.println("Your command has not enough arguments!");
@@ -122,18 +127,24 @@ public class Main {
             System.out.println("Error: " + e);
             System.out.println("Line skipped.");
         }
-        return count;
     }
 
+    public static void deleteTaskList(ArrayList<Task> taskList, int index) throws TaskOutOfRangeException {
+        if (index < 0 || index >= taskList.size()) {
+            throw new TaskOutOfRangeException();
+        } else {
+            taskList.remove(index);
+        }
+    }
     // function to ask the user for an input
-    private static Task[] ask(Task[] inputList) {
+    // modifies contents of Task list depending on user input
+    private static void ask() {
         /* if "bye", loop terminates
          * if "list", shows previous inputs as a numbered list
          * if "mark"/"unmark", mark/unmark task accordingly
          * if "deadline"/"todo"/"event", add task accordingly
+         * if "delete", deletes task accordingly
          */
-        int count = tasksArrLength;
-
         System.out.print("Yoda. Do or do not what shall I help you with? > ");
         String userInput = SCANNER.nextLine();
         while (!userInput.equals("bye")) {
@@ -141,28 +152,52 @@ public class Main {
 
             switch (keywordsArray[0]) {
                 case "list":
-                    printTaskList(inputList, count);
+                    printTaskList(inputList);
                     break;
                 case "mark":
                 case "unmark":
                     try {
                         int itemId = Integer.parseInt(keywordsArray[1]) - 1;
-                        if (itemId >= count) {
+                        if (itemId >= inputList.size()) {
                             throw new TaskOutOfRangeException();
                         }
                         boolean isMark = keywordsArray[0].equals("mark");
-                        markTaskList(inputList, count, itemId, isMark);
+                        markTaskList(inputList, itemId, isMark);
                     } catch (NumberFormatException e) {
                         System.out.println("Input is invalid! Try to mark/unmark again.");
                     } catch (TaskOutOfRangeException e) {
                         System.out.println("Task is out of range!");
-                        System.out.println("You have " + count + " tasks.");
+                        System.out.println("You have " + inputList.size() + " tasks.");
                     }
                     break;
                 case "todo":
                 case "deadline":
                 case "event":
-                    count = addTaskList(inputList, keywordsArray, count, true);
+                    addTaskList(keywordsArray, true);
+                    break;
+                case "delete":
+                    try {
+                        int itemId = Integer.parseInt(keywordsArray[1]) - 1;
+                        String TaskCache = String.valueOf(inputList.get(itemId));
+                        // confirmation message
+                        System.out.println("This task will be PERMANENTLY deleted:");
+                        System.out.println(TaskCache);
+                        System.out.print("Are you sure? (Y/n) > ");
+                        userInput = SCANNER.nextLine();
+
+                        if (userInput.equals("Y")) {
+                            deleteTaskList(inputList, itemId);
+                            System.out.println("Successfully deleted!");
+                        } else {
+                            System.out.println("Delete command aborted...");
+                        }
+
+                    } catch (NumberFormatException e) {
+                        System.out.println("Input is invalid! Try to delete again.");
+                    } catch (TaskOutOfRangeException | IndexOutOfBoundsException e) {
+                        System.out.println("Task is out of range!");
+                        System.out.println("You have " + inputList.size() + " tasks.");
+                    }
                     break;
                 default:
                     System.out.println("Your instruction is invalid.");
@@ -172,32 +207,27 @@ public class Main {
             userInput = SCANNER.nextLine();
         }
 
-        tasksArrLength = count;
-        return inputList;
     }
 
     // function extracts and processes all tasks to tasks array from file f
     // returns a Task[] array, filled with extracted tasks
-    private static Task[] fileToArray(File f) throws FileNotFoundException {
-        Task[] tempArray = new Task[MAX_TASKS];
+    private static void fileToArray(File f) throws FileNotFoundException {
         Scanner fileScanner = new Scanner(f);
 
         while (fileScanner.hasNext()) {
             String fileInput = fileScanner.nextLine();
             String[] keywordsArray = splitInput(fileInput);
 
-            tasksArrLength = addTaskList(tempArray, keywordsArray, tasksArrLength, false);
+            addTaskList(keywordsArray, false);
         }
-
-        return tempArray;
     }
 
-    // function takes a tasks array and writes it to file f
-    private static void arrayToFile(Task[] tasksArr, File f) throws IOException {
+    // function uses task array and writes it to file f
+    private static void arrayToFile(File f) throws IOException {
         FileWriter fWrite = new FileWriter(f);
 
-        for (int i = 0; i < tasksArrLength; i++) {
-            fWrite.write(tasksArr[i].toCommand());
+        for (int i = 0; i < inputList.size(); i++) {
+            fWrite.write(inputList.get(i).toCommand());
             fWrite.write("\n");
         }
         fWrite.close();
@@ -210,7 +240,6 @@ public class Main {
             System.out.println("------------- YODA AWAKENS -------------");
 
 
-            Task[] tasksArrayFromFile;
 
             // creates dir "data/" if it does not exist
             // does nothing if it exists
@@ -222,14 +251,13 @@ public class Main {
 
             if (userFile.createNewFile()) {
                 System.out.println("Greetings youngling, Yoda is my name");
-                tasksArrayFromFile = new Task[MAX_TASKS];
             } else {
                 System.out.println("Welcome back youngling! It is a pleasure to see you again");
-                tasksArrayFromFile = fileToArray(userFile);
+                fileToArray(userFile);
             }
 
-            Task[] newtasksArray = ask(tasksArrayFromFile);
-            arrayToFile(newtasksArray, userFile);
+            ask();
+            arrayToFile(userFile);
 
             System.out.print("\n");
             System.out.println("Do or do not, I shall say goodbye.");
