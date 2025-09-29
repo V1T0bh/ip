cc6431c7c3c61db0db9394c67f9f54c37a5cd8c2
Vito Benedict Halim <vitobenedicthalim@gmail.com>
2025-09-29 18:06:56 +0800

    add file read feature

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,23 @@
 import javax.naming.InsufficientResourcesException;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.util.Scanner;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.*;
 
 public class Main {
     // CONSTANTS
     static final Scanner SCANNER = new Scanner(System.in); //  used for user input
     static final int MAX_TASKS = 100; // max no. of tasks
 
+    private static int tasksArrLength;
+
     // function to split the input to array
     // [TASK TYPE, TASK LABEL, TASK START, TASK END], if exists
     private static String[] splitInput(String userInput) {
-        String[] finalArray = {"", "", "", ""};
+        final int NO_OF_KEYWORDS = 5;
+        String[] finalArray = {"", "", "", "", ""};
         String[] splitArray = userInput.split(" ");
 
         // TYPE is first word
@@ -24,7 +29,7 @@ public class Main {
 
         // LABEL, START, END
         int i = 1;
-        for (int j = 1; j < 4; j++) {
+        for (int j = 1; j < NO_OF_KEYWORDS; j++) {
             boolean hasExceedLength = i >= splitArray.length;
             boolean wordContainsSlash = hasExceedLength || splitArray[i].contains("/");
             while (!(hasExceedLength || wordContainsSlash)) {
@@ -71,8 +76,9 @@ public class Main {
     // function that adds a task to a task list
     // prints either a success or error message
     // returns the new value of count
-    private static int addTaskList(Task[] taskList, String[] keywordsArray, int count) {
+    private static int addTaskList(Task[] taskList, String[] keywordsArray, int count, boolean show) {
         try {
+            int lastIndex = 0;
             Task newTask = new Task();
             if (keywordsArray[1].isEmpty()) {
                 throw new InsufficientResourcesException();
@@ -80,17 +86,28 @@ public class Main {
             switch (keywordsArray[0]) {
                 case "todo":
                     newTask = new Todo(keywordsArray[1]);
+                    lastIndex = 2;
                     break;
                 case "deadline":
                     newTask = new Deadline(keywordsArray[1], keywordsArray[2]);
+                    lastIndex = 3;
                     break;
                 case "event":
                     newTask = new Event(keywordsArray[1], keywordsArray[2], keywordsArray[3]);
+                    lastIndex = 4;
                     break;
             }
             taskList[count] = newTask;
-            System.out.println("Successfully added: ");
-            System.out.println(taskList[count]);
+            if (show) {
+                // expected: entry through user input
+                System.out.println("Successfully added: ");
+                System.out.println(taskList[count]);
+            } else {
+                // expected: entry through file input
+                if (keywordsArray[lastIndex].equals("X")) {
+                    taskList[count].setMarkSilent(true);
+                }
+            }
             count += 1;
         } catch (InsufficientResourcesException e) {
             System.out.print("SHEESHHH!! ");
@@ -103,12 +120,14 @@ public class Main {
     }
 
     // function to ask the user for an input
-    private static Task[] ask(Task[] inputList, int count) {
+    private static Task[] ask(Task[] inputList) {
         /* if "bye", loop terminates
          * if "list", shows previous inputs as a numbered list
          * if "mark"/"unmark", mark/unmark task accordingly
          * if "deadline"/"todo"/"event", add task accordingly
          */
+        int count = tasksArrLength;
+
         System.out.print("Yoda. Do or do not what shall I help you with? > ");
         String userInput = SCANNER.nextLine();
         while (!userInput.equals("bye")) {
@@ -137,7 +156,7 @@ public class Main {
                 case "todo":
                 case "deadline":
                 case "event":
-                    count = addTaskList(inputList, keywordsArray, count);
+                    count = addTaskList(inputList, keywordsArray, count, true);
                     break;
                 default:
                     System.out.println("Your instruction is invalid.");
@@ -150,39 +169,74 @@ public class Main {
         return inputList;
     }
 
-    private static Task[] file_to_array(File f){
+    // function extracts and processes all tasks to tasks array from file f
+    // returns a Task[] array, filled with extracted tasks
+    private static Task[] fileToArray(File f) throws FileNotFoundException {
         Task[] tempArray = new Task[MAX_TASKS];
+        Scanner fileScanner = new Scanner(f);
+
+        while (fileScanner.hasNext()) {
+            String fileInput = fileScanner.nextLine();
+            String[] keywordsArray = splitInput(fileInput);
+
+            tasksArrLength = addTaskList(tempArray, keywordsArray, tasksArrLength, false);
+        }
 
         return tempArray;
     }
 
-    private static void array_to_file(Task[] tasksArr){
+    // function takes a tasks array and writes it to file f
+    private static void arrayToFile(Task[] tasksArr, File f) throws IOException {
+        /*FileWriter fWrite = new FileWriter(f);
+
+
+
+        //fWrite.write("Files in Java might be tricky, but it is fun enough!");
+        fWrite.close();  // must close manually
+        System.out.println("Successfully wrote to the file.");*/
+
         System.out.println("<Array converted to file. Success!>");
     }
 
 
     // main function that runs all other sub-functions
-    public static void main(String[] args) {
-        Task[] tasksArrayFromFile;
-        int tasksArrLength = 0;
+    public static void main(String[] args) throws IOException {
+        try {
+            System.out.println("------------- YODA AWAKENS -------------");
 
-        File userFile = new File("data/user.txt");
-        if (userFile.exists()) {
-            System.out.println("<File found. File read.>");
-            tasksArrayFromFile = file_to_array(userFile);
-        } else {
-            System.out.println("<File not found. File created>"); // create file
-            tasksArrayFromFile = new Task[MAX_TASKS];
+
+            Task[] tasksArrayFromFile;
+
+            // creates dir "data/" if it does not exist
+            // does nothing if it exists
+            Path dir  = Paths.get("data");
+            Files.createDirectories(dir);
+
+            File userFile = new File("data/user.txt");
+            tasksArrLength = 0;
+
+            if (userFile.createNewFile()) {
+                System.out.println("Greetings youngling, Yoda is my name");
+                tasksArrayFromFile = new Task[MAX_TASKS];
+            } else {
+                System.out.println("Welcome back youngling! It is a pleasure to see you again");
+                tasksArrayFromFile = fileToArray(userFile);
+            }
+
+            Task[] newtasksArray = ask(tasksArrayFromFile);
+            arrayToFile(tasksArrayFromFile, userFile);
+
+            System.out.print("\n");
+            System.out.println("Do or do not, I shall say goodbye.");
+            System.out.println("------------- PROGRAM TERMINATED -------------");
+        } catch (IOException e) {
+            System.out.println("There were issues with the file.");
+            System.out.print("ERROR MESSAGE: ");
+            System.out.println(e);
+        } catch (Exception e) {
+            System.out.println("Something went wrong!");
+            System.out.print("ERROR MESSAGE: ");
+            System.out.println(e);
         }
-
-        System.out.println("------------- YODA AWAKENS -------------");
-        System.out.println("Greetings youngling, Yoda is my name");
-
-        Task[] newtasksArray = ask(tasksArrayFromFile, tasksArrLength);
-        array_to_file(tasksArrayFromFile);
-
-        System.out.print("\n");
-        System.out.println("Do or do not, I shall say goodbye.");
-        System.out.println("------------- PROGRAM TERMINATED -------------");
     }
 }
